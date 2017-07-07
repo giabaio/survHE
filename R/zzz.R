516f8eca157d5eb433ccef0b4796b247b5abfe35
Gianluca Baio <g.baio@ucl.ac.uk>
2017-07-07 15:23:17 +0100

    modified:   R/stanmodels.R 	modified:   R/zzz.R 	modified:   src/Makevars 	modified:   src/include/models.hpp 	modified:   src/init.cpp

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  modules <- paste0("stan_fit4", names(stanmodels), "_mod")
-  for (m in modules) loadModule(m, what = TRUE)
+if (!("methods" %in% .packages())) attachNamespace("methods")
+modules <- paste0("stan_fit4", names(stanmodels), "_mod")
+for (m in modules) loadModule(m, what = TRUE)
 }
