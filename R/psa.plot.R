6b91271aea3d5c9c1172be732c0bf07525ee4ed0
Gianluca Baio <g.baio@ucl.ac.uk>
2017-11-28 16:45:08 +0000

    Fix psa.plot

--- R/psa.plot.R
+++ R/psa.plot.R
@@ -54,11 +54,13 @@ psa.plot <- function(psa,...) {
   if(!exists("offset",where=exArgs)) {off <- seq(1,nrow(psa$des.mat))*.35} else {off <- seq(1,nrow(psa$des.mat))*exArgs$offset}
   if(!exists("nsmall",where=exArgs)) {nsmall <- 2} else {nsmall <- exArgs$nsmall}
   if(!exists("digits",where=exArgs)) {digits <- 5} else {digits <- exArgs$digits}
+  if(exists("xlim",where=exArgs)) {xlim=exArgs$xlim} else {xlim <- range(pretty(times))}
+  print(xlim)
   
   # If there's only the average value for the survival curve, simpler plot
   if (psa$nsim==1) {
     alpha <- 1
-    plot(psa$S[[1]][[1]][,1:2],t="l",xlab=xlab,ylab=ylab,col=adjustcolor(col[1],alpha),ylim=c(0,1),xlim=range(pretty(times)),
+    plot(psa$S[[1]][[1]][,1:2],t="l",xlab=xlab,ylab=ylab,col=adjustcolor(col[1],alpha),ylim=c(0,1),xlim=xlim,
          main=main,axes=F)
     if (n.elements>1) {
       pts2 <- lapply(2:n.elements,function(i) points(psa$S[[1]][[i]],t="l",col=adjustcolor(col[i],alpha)))
@@ -71,7 +73,7 @@ psa.plot <- function(psa,...) {
     q025 <- lapply(1:n.elements, function(j) apply(tmp[[j]],2,function(x) quantile(x,.025)))
     q500 <- lapply(1:n.elements, function(j) apply(tmp[[j]],2,function(x) quantile(x,.5))) 
     q975 <- lapply(1:n.elements, function(j) apply(tmp[[j]],2,function(x) quantile(x,.975))) 
-    plot(psa$S[[1]][[1]][,1],q500[[1]],col=adjustcolor(col[1],1),t="l",xlab=xlab,ylab=ylab,ylim=c(0,1),xlim=range(pretty(times)),lwd=2,main=main,axes=F)
+    plot(psa$S[[1]][[1]][,1],q500[[1]],col=adjustcolor(col[1],1),t="l",xlab=xlab,ylab=ylab,ylim=c(0,1),xlim=xlim,lwd=2,main=main,axes=F)
     polygon(c(psa$S[[1]][[1]][,1],rev(psa$S[[1]][[1]][,1])),c(q975[[1]],rev(q025[[1]])),col=adjustcolor(col[1],alpha),border=NA)
     if (n.elements==1) {
     }
@@ -84,14 +86,16 @@ psa.plot <- function(psa,...) {
   }
   axis(1)
   axis(2)
-  if (labs==TRUE) {
+  if (class(labs)=="logical") {
     txt1 <- lapply(1:ncol(psa$des.mat),function(i) {
-      text(xpos,ypos-(i-1)/40,paste0(colnames(psa$des.mat)[i]," : "),cex=cex.txt,pos=2,col="black")
+      text(xpos,ypos-(i-1)/40,paste0(colnames(psa$des.mat)[i]," :  "),cex=cex.txt,pos=2,col="black")
     })
     txt2 <- lapply(1:ncol(psa$des.mat),function(i) {
       lapply(1:nrow(psa$des.mat),function(j) {
         text(xpos+off[j],ypos-(i-1)/40,format(psa$des.mat[j,i],nsmall=nsmall,digits=digits),cex=cex.txt,pos=2,col=col[j])
       })
     })
+  } else {
+     legend("topright",legend=labs,col=col,bty="n",lty=1,cex=cex.txt)
   }
 }
