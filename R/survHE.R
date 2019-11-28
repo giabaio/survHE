2d2b96f00e29ad6ef3144e0323ac49cd9df916f4
Andrea Berardi <andrea.berardi@parexel.com>
2019-11-28 15:50:45 +0000

    Code reflown, 1 typo fixed

--- R/survHE.R
+++ R/survHE.R
@@ -664,7 +664,7 @@ fit.models <- function(formula = NULL, data , distr = NULL, method = "mle", ...)
         data.stan$sigma_Q = 100
       } else if (distr3[x] %in% c("gam", "llo", "wei", "wph")) {
         data.stan$a_alpha = data.stan$b_alpha = 0.1
-      } else if (distr[x] %in% c("lno", "gom")) {
+      } else if (distr3[x] %in% c("lno", "gom")) {
         data.stan$a_alpha = 0
         data.stan$b_alpha = 5
       }
@@ -732,146 +732,202 @@ fit.models <- function(formula = NULL, data , distr = NULL, method = "mle", ...)
 	      linpred.hat <- beta.hat%*%t(data.stan$X)
 	    }
 
-	    if(distr3[i]=="exp") {
-	      logf <- matrix(unlist(lapply(1:nrow(linpred),function(i) {
-	        data.stan$d*log(hexp(data.stan$t,exp(linpred[i,]))) + log(1-pexp(data.stan$t,exp(linpred[i,])))
-	      })),nrow=nrow(linpred),byrow=T)
-	      logf.hat <- matrix(data.stan$d*log(hexp(data.stan$t,exp(linpred.hat))) + log(1-pexp(data.stan$t,exp(linpred.hat))),nrow=1)
+	    if (distr3[i] == "exp") {
+	      logf <- matrix(
+	        unlist(lapply(1:nrow(linpred), function(i) {
+	          data.stan$d * log(hexp(data.stan$t, exp(linpred[i,]))) +
+	            log(1 - pexp(data.stan$t, exp(linpred[i,])))
+	        })),
+	        nrow = nrow(linpred), byrow = T)
+	      logf.hat <- matrix(
+	        data.stan$d * log(hexp(data.stan$t, exp(linpred.hat))) +
+	          log(1 - pexp(data.stan$t, exp(linpred.hat))),
+	        nrow = 1)
 	      # Number of parameters (for AIC): rate + covariates
-	      npars <- 1+sum(1-apply(data.stan$X,2,function(x) all(x==0)))
-	    }
-
-	    if (distr3[i]=="wei") {
-	        shape <- as.numeric(rstan::extract(mod[[i]]$out)$alpha)
-	        shape.hat <- median(shape)
-	        logf <- matrix(unlist(lapply(1:nrow(linpred),function(i) {
-	          data.stan$d*log(hweibull(data.stan$t,shape[i],exp(linpred[i,]))) + log(1-pweibull(data.stan$t,shape[i],exp(linpred[i,])))
-	        })),nrow=nrow(linpred),byrow=T)
-	        logf.hat <- matrix(data.stan$d*log(hweibull(data.stan$t,shape.hat,exp(linpred.hat))) + log(1-pweibull(data.stan$t,shape.hat,exp(linpred.hat))),nrow=1)
-	        # Number of parameters (for AIC): shape, scale + covariates
-	        npars <- 2+sum(1-apply(data.stan$X,2,function(x) all(x==0)))
-	    }
-	    
-	    if (distr3[i]=="wph") {
+	      npars <- 1 + sum(1 - apply(data.stan$X, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "wei") {
 	      shape <- as.numeric(rstan::extract(mod[[i]]$out)$alpha)
-	      shape.hat=median(shape)
-	      logf <- matrix(unlist(lapply(1:nrow(linpred),function(i) {
-	        data.stan$d*log(hweibullPH(data.stan$t,shape[i],exp(linpred[i,]))) + 
-	          log(1-pweibullPH(data.stan$t,shape[i],exp(linpred[i,])))
-	      })),nrow=nrow(linpred),byrow=T)
-	      logf.hat <- matrix(data.stan$d*log(hweibullPH(data.stan$t,shape.hat,exp(linpred.hat)))+
-	                          log(1-pweibullPH(data.stan$t,shape.hat,exp(linpred.hat))),nrow=1)
+	      shape.hat <- median(shape)
+	      logf <- matrix(
+	        unlist(lapply(1:nrow(linpred), function(i) {
+	          data.stan$d * log(hweibull(data.stan$t, shape[i], exp(linpred[i, ]))) + 
+	            log(1 - pweibull(data.stan$t, shape[i], exp(linpred[i, ])))
+	        })), 
+	        nrow = nrow(linpred), byrow = T)
+	      logf.hat <- matrix(
+	        data.stan$d * log(hweibull(data.stan$t, shape.hat, exp(linpred.hat))) + 
+	          log(1 - pweibull(data.stan$t, shape.hat, exp(linpred.hat))), 
+	        nrow = 1)
 	      # Number of parameters (for AIC): shape, scale + covariates
-	      npars <- 2+sum(1-apply(data.stan$X,2,function(x) all(x==0)))
-	    }
-
-	    if (distr3[i]=="gom") {
+	      npars <- 2 + sum(1 - apply(data.stan$X, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "wph") {
 	      shape <- as.numeric(rstan::extract(mod[[i]]$out)$alpha)
-	      shape.hat=median(shape)
-	      logf <- matrix(unlist(lapply(1:nrow(linpred),function(i) {
-	        data.stan$d*log(hgompertz(data.stan$t,shape=shape[i],rate=exp(linpred[i,]))) + 
-	          log(1-pgompertz(shape=data.stan$t,shape[i],rate=exp(linpred[i,])))
-	      })),nrow=nrow(linpred),byrow=T)
-	      logf.hat <- matrix(data.stan$d*log(hgompertz(data.stan$t,shape.hat,exp(linpred.hat)))+
-	                          log(1-pgompertz(data.stan$t,shape.hat,exp(linpred.hat))),nrow=1)
+	      shape.hat = median(shape)
+	      logf <- matrix(
+	        unlist(lapply(1:nrow(linpred), function(i) {
+	        data.stan$d * log(hweibullPH(data.stan$t, shape[i], exp(linpred[i, ]))) +
+	          log(1 - pweibullPH(data.stan$t, shape[i], exp(linpred[i, ])))
+	      })), nrow = nrow(linpred), byrow = T)
+	      logf.hat <- matrix(
+	        data.stan$d * log(hweibullPH(data.stan$t, shape.hat, exp(linpred.hat))) +
+	          log(1 - pweibullPH(data.stan$t, shape.hat, exp(linpred.hat))), 
+	        nrow = 1)
+	      # Number of parameters (for AIC): shape, scale + covariates
+	      npars <- 2 + sum(1 - apply(data.stan$X, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "gom") {
+	      shape <- as.numeric(rstan::extract(mod[[i]]$out)$alpha)
+	      shape.hat = median(shape)
+	      logf <- matrix(
+	        unlist(lapply(1:nrow(linpred), function(i) {
+	          data.stan$d * log(hgompertz(data.stan$t, shape = shape[i], rate = exp(linpred[i, ]))) +
+	            log(1 - pgompertz(shape = data.stan$t, shape[i], rate = exp(linpred[i, ])))
+	        })), 
+	        nrow = nrow(linpred), byrow = T)
+	      logf.hat <- matrix(
+	        data.stan$d * log(hgompertz(data.stan$t, shape.hat, exp(linpred.hat))) +
+	          log(1 - pgompertz(data.stan$t, shape.hat, exp(linpred.hat))),
+	        nrow = 1)
 	      # Number of parameters (for AIC): shape, rate + covariates
-	      npars <- 2+sum(1-apply(data.stan$X,2,function(x) all(x==0)))
-	    }
-	    
-	    if (distr3[i]=="gam") {
-	        shape <- as.numeric(rstan::extract(mod[[i]]$out)$alpha)
-	        shape.bar <- median(shape)
-	        lo <- exp(beta%*%t(data.stan$X_obs))
-	        lc <- exp(beta%*%t(data.stan$X_cens))
-	        lo.bar <- exp(beta.hat%*%t(data.stan$X_obs))
-	        lc.bar <- exp(beta.hat%*%t(data.stan$X_cens))
-	        f=matrix(unlist(lapply(1:nrow(lo),function(i) dgamma(data.stan$t,shape[i],lo[i,]))),nrow=nrow(lo),byrow=T)
-	        f.bar=matrix(unlist(lapply(1:nrow(lo.bar),function(i) dgamma(data.stan$t,shape.bar,lo.bar[i,]))),nrow=1,byrow=T)
-	        s=matrix(unlist(lapply(1:nrow(lc),function(i) 1-pgamma(data.stan$d,shape[i],lc[i,]))),nrow=nrow(lc),byrow=T)
-	        s.bar=matrix(unlist(lapply(1:nrow(lc.bar),function(i) 1-pgamma(data.stan$d,shape.bar,lc.bar[i,]))),nrow=1,byrow=T)
-	        # Number of parameters (for AIC): shape, rate + covariates
-	        npars <- 2+sum(1-apply(data.stan$X_obs,2,function(x) all(x==0)))
-	    }
-	    
-	    if (distr3[i]=="gga") {
-	        q=as.numeric(rstan::extract(mod[[i]]$out)$Q)
-	        q.bar=median(q)
-	        scale=as.numeric(rstan::extract(mod[[i]]$out)$sigma)
-	        scale.bar=median(scale)
-	        lo=(beta%*%t(data.stan$X_obs))
-	        lc=(beta%*%t(data.stan$X_cens))
-	        lo.bar=(beta.hat%*%t(data.stan$X_obs))
-	        lc.bar=(beta.hat%*%t(data.stan$X_cens))
-	        f=matrix(unlist(lapply(1:nrow(lo),function(i) dgengamma(data.stan$t,lo[i,],scale[i],q[i]))),nrow=nrow(lo),byrow=T)
-	        f.bar=matrix(unlist(lapply(1:nrow(lo.bar),function(i) dgengamma(data.stan$t,lo.bar[i,],scale.bar,q.bar))),nrow=1,byrow=T)
-	        s=matrix(unlist(lapply(1:nrow(lc),function(i) 1-pgengamma(data.stan$d,lc[i,],scale[i],q[i]))),nrow=nrow(lc),byrow=T)
-	        s.bar=matrix(unlist(lapply(1:nrow(lc.bar),function(i) 1-pgengamma(data.stan$d,lc.bar[i,],scale.bar,q.bar))),nrow=1,byrow=T)
-	        # Number of parameters (for AIC): mu, sigma, Q + covariates
-	        npars <- 3+sum(1-apply(data.stan$X_obs,2,function(x) all(x==0)))
-	    }
-	    
-	    if (distr3[i]=="gef") {
-	        Q=as.numeric(rstan::extract(mod[[i]]$out)$Q)
-	        Q.bar=median(Q)
-	        P=as.numeric(rstan::extract(mod[[i]]$out)$P)
-	        P.bar=median(P)
-	        sigma=as.numeric(rstan::extract(mod[[i]]$out)$sigma)
-	        sigma.bar=mean(sigma)
-	        lo=(beta%*%t(data.stan$X_obs))
-	        lc=(beta%*%t(data.stan$X_cens))
-	        lo.bar=(beta.hat%*%t(data.stan$X_obs))
-	        lc.bar=(beta.hat%*%t(data.stan$X_cens))
-	        f=matrix(unlist(lapply(1:nrow(lo),function(i) dgenf(data.stan$t,lo[i,],sigma[i],Q[i],P[i]))),nrow=nrow(lo),byrow=T)
-	        f.bar=matrix(unlist(lapply(1:nrow(lo.bar),function(i) dgenf(data.stan$t,lo.bar[i,],sigma.bar,Q.bar,P.bar))),nrow=1,byrow=T)
-	        s=matrix(unlist(lapply(1:nrow(lc),function(i) 1-pgenf(data.stan$d,lc[i,],sigma[i],Q[i],P[i]))),nrow=nrow(lc),byrow=T)
-	        s.bar=matrix(unlist(lapply(1:nrow(lc.bar),function(i) 1-pgenf(data.stan$d,lc.bar[i,],sigma.bar,Q.bar,P.bar))),nrow=1,byrow=T)
-	        # Number of parameters (for AIC): mu, sigma, Q, P + covariates
-	        npars <- 4+sum(1-apply(data.stan$X_obs,2,function(x) all(x==0)))
-	    }
-	    
-	    if (distr3[i]=="lno") {
-	      sigma=as.numeric(rstan::extract(mod[[i]]$out)$alpha)
-	      sigma.hat=median(sigma)
-	      logf <- matrix(unlist(lapply(1:nrow(linpred),function(i) {
-	        data.stan$d*log(hlnorm(data.stan$t,(linpred[i,]),sigma[i])) + 
-	          log(1-plnorm(data.stan$t,(linpred[i,]),sigma[i]))
-	      })),nrow=nrow(linpred),byrow=T)
-	      logf.hat <- matrix(data.stan$d*log(hlnorm(data.stan$t,(linpred.hat),sigma.hat))+
-	                          log(1-plnorm(data.stan$t,(linpred.hat),sigma.hat)),nrow=1)
+	      npars <- 2 + sum(1 - apply(data.stan$X, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "gam") {
+	      shape <- as.numeric(rstan::extract(mod[[i]]$out)$alpha)
+	      shape.bar <- median(shape)
+	      lo <- exp(beta %*% t(data.stan$X_obs))
+	      lc <- exp(beta %*% t(data.stan$X_cens))
+	      lo.bar <- exp(beta.hat %*% t(data.stan$X_obs))
+	      lc.bar <- exp(beta.hat %*% t(data.stan$X_cens))
+	      f = matrix(
+	        unlist(lapply(1:nrow(lo), function(i) 
+	          dgamma(data.stan$t, shape[i], lo[i, ]))),
+	        nrow = nrow(lo), byrow = T)
+	      f.bar = matrix(
+	        unlist(lapply(1:nrow(lo.bar), function(i)
+	          dgamma(data.stan$t, shape.bar, lo.bar[i, ]))), 
+	        nrow = 1, byrow = T)
+	      s = matrix(
+	        unlist(lapply(1:nrow(lc), function(i) 
+	          1 - pgamma(data.stan$d, shape[i], lc[i, ]))),
+	        nrow = nrow(lc), byrow = T)
+	      s.bar = matrix(
+	        unlist(lapply(1:nrow(lc.bar), function(i) 
+	          1 - pgamma(data.stan$d, shape.bar, lc.bar[i, ]))),
+	        nrow = 1, byrow = T)
+	      # Number of parameters (for AIC): shape, rate + covariates
+	      npars <- 2 + sum(1 - apply(data.stan$X_obs, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "gga") {
+	      q = as.numeric(rstan::extract(mod[[i]]$out)$Q)
+	      q.bar = median(q)
+	      scale = as.numeric(rstan::extract(mod[[i]]$out)$sigma)
+	      scale.bar = median(scale)
+	      lo = (beta %*% t(data.stan$X_obs))
+	      lc = (beta %*% t(data.stan$X_cens))
+	      lo.bar = (beta.hat %*% t(data.stan$X_obs))
+	      lc.bar = (beta.hat %*% t(data.stan$X_cens))
+	      f = matrix(
+	        unlist(lapply(1:nrow(lo), function(i)
+	          dgengamma(data.stan$t, lo[i, ], scale[i], q[i]))), 
+	        nrow = nrow(lo), byrow = T)
+	      f.bar = matrix(
+	        unlist(lapply(1:nrow(lo.bar), function(i)
+	          dgengamma(data.stan$t, lo.bar[i, ], scale.bar, q.bar))), 
+	        nrow = 1, byrow = T)
+	      s = matrix(
+	        unlist(lapply(1:nrow(lc), function(i)
+	          1 - pgengamma(data.stan$d, lc[i, ], scale[i], q[i]))), 
+	        nrow = nrow(lc), byrow = T)
+	      s.bar = matrix(
+	        unlist(lapply(1:nrow(lc.bar), function(i) 
+	          1 - pgengamma(data.stan$d, lc.bar[i, ], scale.bar, q.bar))), 
+	        nrow = 1, byrow = T)
+	      # Number of parameters (for AIC): mu, sigma, Q + covariates
+	      npars <- 3 + sum(
+	        1 - apply(data.stan$X_obs, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "gef") {
+	      Q = as.numeric(rstan::extract(mod[[i]]$out)$Q)
+	      Q.bar = median(Q)
+	      P = as.numeric(rstan::extract(mod[[i]]$out)$P)
+	      P.bar = median(P)
+	      sigma = as.numeric(rstan::extract(mod[[i]]$out)$sigma)
+	      sigma.bar = mean(sigma)
+	      lo = (beta %*% t(data.stan$X_obs))
+	      lc = (beta %*% t(data.stan$X_cens))
+	      lo.bar = (beta.hat %*% t(data.stan$X_obs))
+	      lc.bar = (beta.hat %*% t(data.stan$X_cens))
+	      f = matrix(
+	        unlist(lapply(1:nrow(lo), function(i) 
+	          dgenf(data.stan$t, lo[i, ], sigma[i], Q[i], P[i]))), 
+	        nrow = nrow(lo), byrow = T)
+	      f.bar = matrix(
+	        unlist(lapply(1:nrow(lo.bar), function(i) 
+	          dgenf(data.stan$t, lo.bar[i, ], sigma.bar, Q.bar, P.bar))), 
+	        nrow = 1, byrow = T)
+	      s = matrix(
+	        unlist(lapply(1:nrow(lc), function(i)
+	          1 - pgenf(data.stan$d, lc[i, ], sigma[i], Q[i], P[i]))),
+	        nrow = nrow(lc), byrow = T)
+	      s.bar = matrix(
+	        unlist(lapply(1:nrow(lc.bar), function(i) 
+	          1 - pgenf(data.stan$d, lc.bar[i, ], sigma.bar, Q.bar, P.bar))),
+	        nrow = 1, byrow = T)
+	      # Number of parameters (for AIC): mu, sigma, Q, P + covariates
+	      npars <- 4 + sum(
+	        1 - apply(data.stan$X_obs, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "lno") {
+	      sigma = as.numeric(rstan::extract(mod[[i]]$out)$alpha)
+	      sigma.hat = median(sigma)
+	      logf <- matrix(
+	        unlist(lapply(1:nrow(linpred), function(i) {
+	          data.stan$d * log(hlnorm(data.stan$t, (linpred[i, ]), sigma[i])) +
+	            log(1 - plnorm(data.stan$t, (linpred[i, ]), sigma[i]))
+	        })), nrow = nrow(linpred), byrow = T)
+	      logf.hat <- matrix(
+	        data.stan$d * log(hlnorm(data.stan$t, (linpred.hat), sigma.hat)) +
+	          log(1 - plnorm(data.stan$t, (linpred.hat), sigma.hat)),
+	        nrow = 1)
 	      # Number of parameters (for AIC): meanlog, sdlog + covariates
-	      npars <- 2+sum(1-apply(data.stan$X,2,function(x) all(x==0)))
-	    }
-
-	    if (distr3[i]=="llo") {
-	      sigma=as.numeric(rstan::extract(mod[[i]]$out)$alpha)
-	      sigma.hat=median(sigma)
-	      logf <- matrix(unlist(lapply(1:nrow(linpred),function(i) {
-	        data.stan$d*log(hllogis(data.stan$t,sigma[i],exp(linpred[i,]))) + 
-	          log(1-pllogis(data.stan$t,sigma[i],exp(linpred[i,])))
-	      })),nrow=nrow(linpred),byrow=T)
-	      logf.hat <- matrix(data.stan$d*log(hllogis(data.stan$t,sigma.hat,exp(linpred.hat)))+
-	                          log(1-pllogis(data.stan$t,sigma.hat,exp(linpred.hat))),nrow=1)
+	      npars <- 2 + sum(
+	        1 - apply(data.stan$X, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "llo") {
+	      sigma = as.numeric(rstan::extract(mod[[i]]$out)$alpha)
+	      sigma.hat = median(sigma)
+	      logf <- matrix(
+	        unlist(lapply(1:nrow(linpred), function(i) {
+	          data.stan$d * log(hllogis(data.stan$t, sigma[i], exp(linpred[i, ]))) +
+	          log(1 - pllogis(data.stan$t, sigma[i], exp(linpred[i, ])))
+	      })), nrow = nrow(linpred), byrow = T)
+	      logf.hat <- matrix(
+	        data.stan$d * log(hllogis(data.stan$t, sigma.hat, exp(linpred.hat))) +
+	          log(1 - pllogis(data.stan$t, sigma.hat, exp(linpred.hat))),
+	        nrow = 1)
 	      # Number of parameters (for AIC): shape, scale + covariates
-	      npars <- 2+sum(1-apply(data.stan$X,2,function(x) all(x==0)))
-	    }	      
-
-	    if (distr3[i]=="rps") {
-	        gamma <- rstan::extract(mod[[i]]$out)$gamma
-	        gamma.hat <- apply(gamma,2,median)
-	        logf <- data.stan$d*(-log(data.stan$t)+log(gamma%*%t(data.stan$DB)) + gamma%*%t(data.stan$B)+ beta%*%t(data.stan$X)) -
-	                     exp(gamma%*%t(data.stan$B)+ beta%*%t(data.stan$X))
-	        logf.hat <- t(data.stan$d*(-log(data.stan$t)+log(data.stan$DB%*%gamma.hat)+data.stan$B%*%gamma.hat + data.stan$X%*%beta.hat) - 
-	            exp(data.stan$B%*%gamma.hat + data.stan$X%*%beta.hat))
-	        # Number of parameters (for AIC): gamma + covariates
-	        npars <- length(gamma.hat)+sum(apply(data.stan$X,2,function(x) 1-all(x==0)))
-	    }	  
+	      npars <- 2 + sum(
+	        1 - apply(data.stan$X, 2, function(x) all(x == 0)))
+	    } else if (distr3[i] == "rps") {
+	      gamma <- rstan::extract(mod[[i]]$out)$gamma
+	      gamma.hat <- apply(gamma, 2, median)
+	      logf <- data.stan$d * (
+	        -log(data.stan$t) + log(gamma %*% t(data.stan$DB)) + 
+	          gamma %*% t(data.stan$B) +
+	          beta %*% t(data.stan$X)) -
+	        exp(gamma %*% t(data.stan$B) + beta %*% t(data.stan$X))
+	      logf.hat <- t(
+	        data.stan$d * (
+	          -log(data.stan$t) + log(data.stan$DB %*% gamma.hat) + 
+	            data.stan$B %*% gamma.hat + 
+	            data.stan$X %*% beta.hat) -
+	          exp(data.stan$B %*% gamma.hat + data.stan$X %*% beta.hat))
+	      # Number of parameters (for AIC): gamma + covariates
+	      npars <- length(gamma.hat) + sum(
+	        apply(data.stan$X, 2, function(x) 1 - all(x == 0)))
+	    }
 	    
 	    # Now computes the log-likelihood and then deviance and DIC, AIC, BIC
 	    # Little function to compute the log-likelihood (for the obs vs censored cases)
 	    compute.loglik <- function(f,s) {
-	      loglik <- (apply(log(f),1,sum)+apply(log(s),1,sum))
+	      loglik <- (apply(log(f),1,sum) + apply(log(s),1,sum))
 	      return(loglik)
 	    }
+	    
 	    if (distr3[i] %in% c("gam","gga","gef")) {
 	      loglik <- compute.loglik(f,s)
 	      D.theta <- -2*loglik 
@@ -897,16 +953,16 @@ fit.models <- function(formula = NULL, data , distr = NULL, method = "mle", ...)
   # Now defines the outputs of the function
   model.fitting <- list(aic=aic,bic=bic,dic=dic)
   misc <- list(time2run=time2run,formula=formula,km=ObjSurvfit,data=data)
-  if(method=="hmc") {
+  if (method == "hmc") {
     misc$vars <- vars
-    misc$data.stan=lapply(1:length(mod),function(x) mod[[x]]$data.stan)
-    model.fitting$dic2=dic2
+    misc$data.stan = lapply(1:length(mod),function(x) mod[[x]]$data.stan)
+    model.fitting$dic2 = dic2
     # If save.stan is set to TRUE, then saves the Stan model file(s) & data
-    if(save.stan==TRUE) {
+    if (save.stan == TRUE) {
       write_model <- lapply(1:length(distr),function(i) {
         model_code <- attr(mod[[i]]$out@stanmodel,"model_code")
         con <- paste0(distr[i],".stan")
-        writeLines(model_code, con=con)
+        writeLines(model_code, con = con)
         txt <- paste0("Model code saved to the file: ",con,"\n")
         cat(txt)
       })
