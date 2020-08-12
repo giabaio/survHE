ff727dd833683b433973f3d314192544b123f3e7
Gianluca Baio <giabaio@gmail.com>
2020-08-12 12:45:49 +0100

    Work on make.surv + fix compute_ICs_stan

--- R/runINLA.R
+++ R/runINLA.R
@@ -74,14 +74,10 @@ runINLA <- function(x,exArgs) {
   
   # Now re-writes the formula in general terms (without linking to INLA::inla.surv)
   formula <- as.formula(gsub("INLA::inla.surv","Surv",deparse(formula)))
-<<<<<<< HEAD
-  
   # Adds a field used in 'make.surv' to indicate the model used
   model$dlist$name <- d
-=======
->>>>>>> 7d4b9a298f58df2d003722ab17c63b3140a827e8
 
-  # Finally returns the output
+    # Finally returns the output
   list(
     model=model,
     aic=2*model$dic$p.eff+model$dic$deviance.mean,
