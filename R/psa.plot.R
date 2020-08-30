0b5c084d9d3dea05b945fe155ad971dd24a59974
Gianluca Baio <giabaio@gmail.com>
2020-08-31 00:04:17 +0100

    Clean up files

--- R/psa.plot.R
+++ R/psa.plot.R
@@ -1,7 +1,10 @@
 #' Graphical depiction of the probabilistic sensitivity analysis for the
 #' survival curves
 #' 
-#' Plots the survival curves for all the PSA simulations
+#' Plots the survival curves for all the PSA simulations. The function is 
+#' actually deprecated - similar graphs can be obtained directly using 
+#' the \code{plot} method (with options), which allows a finer depiction
+#' of the results. 
 #' 
 #' 
 #' @param psa the result of the call to the function \code{make.surv}
@@ -29,83 +32,73 @@ psa.plot <- function(psa,...) {
   # xlab = label for the x-axis
   # ylab = label for the y-axis
   # col = vector of colours with which to plot the curves
-  # alpha = parameter to determine the transparency (default = 0.1)
+  # alpha = parameter to determine the transparency (default = 0.2)
   # main = a string to write the title
-  # labs = logical (default = TRUE): should text to identify which profile has been plotted printed on the graph?
-  # xpos = the point on the x-axis in which to write the legend with the profiles (default = 65% of the x-axis)
-  # ypos = the point on the y-axis in which to write the legend with the profiles (default = 100% of the y-axis)
-  # cex.txt = the factor by which to write the text (default = .75)
-  # offset = how much space between text for each line? (default = .35)
-  # nsmall = number of decimal places (default = 2)
-  # digits = number of digits used for the numerical values in the labels
+  # labs = a vector with non-standard names for the legend values
+  # name_labs = the non-standard title for the legend
   # xlim = a vector of limits for the times
-  # scale.time = a factor by which to rescale the time variable (eg 1/52 rescales weeks to years)
-  # round.fact = the number of decimal places to include in the x-axis for the time variable (when rescaling!)
   
-  n.elements <- length(psa$S[[1]]) 
-  times <- psa$S[[1]][[1]][,1]
-  exArgs <- list(...)
-  if(!exists("xlab",where=exArgs)) {xlab <- "Time"} else {xlab <- exArgs$xlab}
-  if(!exists("ylab",where=exArgs)) {ylab="Survival"} else {ylab <- exArgs$ylab}
-  if(!exists("col",where=exArgs)) {col <- sample(colors(),n.elements)} else {col <- exArgs$col}
-  if(!exists("alpha",where=exArgs)) {alpha <- 0.1} else {alpha <- exArgs$alpha}
-  if(!exists("main",where=exArgs)) {main <- ""} else {main <- exArgs$main}
-  if(!exists("labs",where=exArgs)) {labs <- TRUE} else {labs <- exArgs$labs}
-  if(!exists("xpos",where=exArgs)) {xpos <- max(times)*0.65} else {xpos <- exArgs$xpos}
-  if(!exists("ypos",where=exArgs)) {ypos <- 1} else {ypos <- exArgs$ypos}
-  if(!exists("cex.txt",where=exArgs)) {cex.txt <- 0.75} else {cex.txt <- exArgs$cex.txt}
-  if(!exists("offset",where=exArgs)) {off <- seq(1,nrow(psa$des.mat))*.35} else {off <- seq(1,nrow(psa$des.mat))*exArgs$offset}
-  if(!exists("nsmall",where=exArgs)) {nsmall <- 2} else {nsmall <- exArgs$nsmall}
-  if(!exists("digits",where=exArgs)) {digits <- 5} else {digits <- exArgs$digits}
-  if(exists("xlim",where=exArgs)) {xlim=exArgs$xlim} else {xlim <- range(pretty(times))}
+  exArgs=list(...)
+
+  # Creates the dataset to plot with the survival curves for all profiles  
+  strata=lapply(1:nrow(psa$des.mat),function(x) {
+    psa$des.mat %>% as_tibble() %>% select(-matches("(Intercept)",everything())) %>% slice(x) %>% 
+      round(digits=2) %>% mutate(strata=paste0(names(.),"=",.,collapse=","))
+  }) %>% bind_rows(.) %>% select(strata)
+  toplot=lapply(1:length(psa$S),function(i) {
+    psa$S[[i]] %>% bind_cols(strata=as.factor(strata[i,]))
+  }) %>% bind_rows(.)
+
+  if(exists("alpha",where=exArgs)){alpha=exArgs$alpha} else {alpha=0.2}
+  if(exists("name_labs",where=exArgs)){name_labs=exArgs$name_labs} else {name_labs="Profile"}
   
-  # If there's only the average value for the survival curve, simpler plot
-  if (psa$nsim==1) {
-    alpha <- 1
-    plot(psa$S[[1]][[1]][,1:2],t="l",xlab=xlab,ylab=ylab,col=adjustcolor(col[1],alpha),ylim=c(0,1),xlim=xlim,
-         main=main,axes=F)
-    if (n.elements>1) {
-      pts2 <- lapply(2:n.elements,function(i) points(psa$S[[1]][[i]],t="l",col=adjustcolor(col[i],alpha)))
-    }
+  psa.plot=ggplot(data=toplot,aes(x=t,y=S,colour=strata))+
+    geom_line(size=.9) +
+    theme_bw() + 
+    theme(axis.text.x = element_text(color="black",size=12,angle=0,hjust=.5,vjust=.5),
+          axis.text.y = element_text(color="black",size=12,angle=0,hjust=.5,vjust=.5),
+          axis.title.x = element_text(color="black",size=14,angle=0,hjust=.5,vjust=.5),
+          axis.title.y = element_text(color="black",size=14,angle=90,hjust=.5,vjust=.5)) +
+    theme(axis.line = element_line(colour = "black"),
+          #panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(),
+          #panel.border = element_blank(),
+          panel.background = element_blank(),
+          panel.border = element_blank(),
+          plot.title = element_text(size=18, face="bold")) +
+    theme(legend.position=c(.75,.9),
+          legend.title=element_text(size=15,face="bold"),
+          #legend.title = element_blank(),
+          legend.text = element_text(colour="black", size=14, face="plain"),
+          legend.background=element_blank()) +
+    labs(y="Survival",x="Time",title=NULL,
+         color=name_labs) 
+
+    # If there are more than 1 simulation, then there also are the low and upp extremes and plots them too
+  if(any(grepl("low",names(toplot)))) {
+    psa.plot=psa.plot+geom_ribbon(data=toplot,aes(x=t,y=S,ymin=low,ymax=upp,fill=strata),alpha=alpha,show.legend=F)
   }
   
-  # If there are nsim simulations from the survival curves, then more complex plot
-  if (psa$nsim>1) {
-    tmp <- lapply(1:n.elements,function(j) matrix(unlist(lapply(1:psa$nsim,function(i) psa$S[[i]][[j]][,2])),nrow=psa$nsim,byrow=T))
-    q025 <- lapply(1:n.elements, function(j) apply(tmp[[j]],2,function(x) quantile(x,.025)))
-    q500 <- lapply(1:n.elements, function(j) apply(tmp[[j]],2,function(x) quantile(x,.5))) 
-    q975 <- lapply(1:n.elements, function(j) apply(tmp[[j]],2,function(x) quantile(x,.975))) 
-    plot(psa$S[[1]][[1]][,1],q500[[1]],col=adjustcolor(col[1],1),t="l",xlab=xlab,ylab=ylab,ylim=c(0,1),xlim=xlim,lwd=2,main=main,axes=F)
-    polygon(c(psa$S[[1]][[1]][,1],rev(psa$S[[1]][[1]][,1])),c(q975[[1]],rev(q025[[1]])),col=adjustcolor(col[1],alpha),border=NA)
-    if (n.elements==1) {
-    }
-    if (n.elements>1) {
-      lapply(2:n.elements, function(i) {
-        pts1 <- points(psa$S[[1]][[i]][,1],q500[[i]],col=adjustcolor(col[i],1),t="l",lwd=2) 
-        pts2 <- polygon(c(psa$S[[1]][[i]][,1],rev(psa$S[[1]][[i]][,1])),c(q975[[i]],rev(q025[[i]])),col=adjustcolor(col[i],alpha),border=NA)
-      })
-    }
+  # Optional arguments
+  if(exists("col",where=exArgs)) {
+    psa.plot=psa.plot+scale_color_manual(values=exArgs$col)+scale_fill_manual(values=exArgs$col)
   }
-  # If the time has been re-scaled, then use the new scale as labels on the x-axis. Otherwise, use original scale
-  if(exists("scale.time",where=exArgs)) {
-      if(exists("round.fact",where=exArgs)) {round.fact=exArgs$round.fact} else {round.fact=1}
-      axis(1,at=pretty(times),labels=round(pretty(times)*exArgs$scale.time,round.fact))
-  } else {
-      axis(1)
+  if(exists("xlab",where=exArgs)){
+    psa.plot=psa.plot+labs(x=exArgs$xlab)
   }
-  axis(2)
-  if (class(labs)=="logical") {
-    if(labs){ 
-       txt1 <- lapply(1:ncol(psa$des.mat),function(i) {
-          text(xpos,ypos-(i-1)/40,paste0(colnames(psa$des.mat)[i]," :  "),cex=cex.txt,pos=2,col="black")
-       })
-       txt2 <- lapply(1:ncol(psa$des.mat),function(i) {
-          lapply(1:nrow(psa$des.mat),function(j) {
-             text(xpos+off[j],ypos-(i-1)/40,format(psa$des.mat[j,i],nsmall=nsmall,digits=digits),cex=cex.txt,pos=2,col=col[j])
-          })
-       })
-    }
-  } else {
-     legend("topright",legend=labs,col=col,bty="n",lty=1,cex=cex.txt)
+  if(exists("ylab",where=exArgs)){
+    psa.plot=psa.plot+labs(y=exArgs$ylab)
   }
+  if(exists("main",where=exArgs)) {
+    psa.plot=psa.plot+labs(title=exArgs$main)
+  }
+  if(exists("labs",where=exArgs)) {
+    psa.plot=psa.plot+scale_color_discrete(labels=exArgs$labs)
+  }
+  if(exists("xlim",where=exArgs)){
+    psa.plot=psa.plot+xlim(exArgs$xlim)
+  }
+  
+  # Finally renders the plot
+  psa.plot
 }
